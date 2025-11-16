2b6f563b54c9522af67baf8ad7740e17053e52c5
Jmini392 <bread0902@gmail.com>
2025-11-16 17:07:59 +0900

    obj파일 변경

--- MiniFarm/Source/House.cpp
+++ MiniFarm/Source/House.cpp
@@ -3,7 +3,7 @@
 
 House::House()
 {
-    m_model = std::make_shared<Model>("Models/house.obj");
+    m_model = std::make_shared<Model>("Models/farm.obj");
     m_pos = { 0.f, 0.f, 0.f };
     m_rot = { 0.f, 0.f, 0.f };
     m_scale = { 1.f, 1.f, 1.f };
