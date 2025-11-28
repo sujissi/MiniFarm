977fa16548caecb80be4016ee54a089d789052d1
sujissi_Laptop <hwangsuji329@gmail.com>
2025-11-29 01:14:36 +0900

    땅 수정

--- MiniFarm/Source/Systems/ObjectFactory.cpp
+++ MiniFarm/Source/Systems/ObjectFactory.cpp
@@ -14,7 +14,7 @@ std::shared_ptr<GameObject> ObjectFactory::Create(
     const ObjectInfo* info
 )
 {
-    if (type == "Field")
+    if (type == "Thick_Ground")
     {
         return std::make_shared<Crop>(pos, rot, scale, info->modelPath, info->texturePath);
     }
